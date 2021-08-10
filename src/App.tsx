95f1f05ae9411116c99d07670f3f092f50b89fab
Rodolfo Gonçalves <OrencioRodolfo@users.noreply.github.com>
2021-08-10 10:24:12 +0100

    add Deploy workflow to github actions

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
+        <p>Hello you</p>
       </header>
     </div>
   );
