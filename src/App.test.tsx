90110df6cf77c2f95942cf5afd96784ac79d30fd
Rodolfo Goncalves <rodolfo.goncalves@blip.pt>
2021-08-10 10:44:38 +0100

    fix unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders "Hello" message', () => {
+test('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello/i);
+  const linkElement = screen.getByText(/Hey you there!/i);
   expect(linkElement).toBeInTheDocument();
 });
