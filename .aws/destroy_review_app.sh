0d3a7066ad56c4ae2e9576cc3accaeca2db130bd
Rodolfo Goncalves <rodolfo.goncalves@blip.pt>
2021-08-12 17:55:18 +0100

    test

--- .aws/destroy_review_app.sh
+++ .aws/destroy_review_app.sh
@@ -1,6 +1,5 @@
 
-bucket_name="github-actions-poc.$(echo $GITHUB_HEAD_REF | sed -e 's/\//-/g')"
-echo $bucket_name;
+bucket_name="github-actions-poc.$(echo $GITHUB_REF | sed -e 's/\//-/g')"
 
 # delete bucket and all its objects
 aws s3 rb s3://$bucket_name --force
