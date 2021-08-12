cfe211179a4a20136552cdd5e5f807881e4e258f
Rodolfo Goncalves <rodolfo.goncalves@blip.pt>
2021-08-12 17:56:11 +0100

    batatas

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hey!</p>
+        <p>Hey batatas</p>
       </header>
     </div>
   );
