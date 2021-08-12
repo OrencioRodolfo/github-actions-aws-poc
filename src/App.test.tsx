06be223a94484313ec1cb547250e3f01583db18c
Rodolfo Goncalves <rodolfo.goncalves@blip.pt>
2021-08-12 16:30:12 +0100

    change welcome message to Rodolfo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders welcome message', () => {
+test("renders welcome message", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hey you there/i);
+  const linkElement = screen.getByText(/Hey Rodolfo!/i);
   expect(linkElement).toBeInTheDocument();
 });
