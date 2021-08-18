f4a48832f594929ff8dccf88bfb5443f6b807e07
Rodolfo Goncalves <rodolfo.goncalves@blip.pt>
2021-08-18 08:51:30 +0100

    change greeting message

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hey Rodolfo!</p>
+        <p>Hey everyone!</p>
       </header>
     </div>
   );
